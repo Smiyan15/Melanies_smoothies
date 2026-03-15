949d5e72a3fd30f5854b6030d9839d4e628e712f
Shubhangi <shubhangimiyan@gmail.com>
2026-03-15 16:35:18 -0500

    Reorganize requests import and API call
    Moved the requests import and API call to a different location in the code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+
 
 
 # Write directly to the app
@@ -41,6 +39,10 @@ if ingredients_list:
     #st.write(my_insert_stmt)
   
     time_to_insert = st.button('Submit Order')
+
+  import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
     
     
     if time_to_insert:
